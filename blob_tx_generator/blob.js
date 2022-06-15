ff3c9a2d7cf3200e91d84f56378df40f77becdbb
Michael de Hoog <michael.dehoog@coinbase.com>
2022-06-15 10:03:54 -0500

    Fix blob submission script

--- blob_tx_generator/blob.js
+++ blob_tx_generator/blob.js
@@ -2,10 +2,11 @@ const ethers = require("ethers")
 const axios = require('axios')
 
 const input = process.argv[2]
+const expected_kzgs = process.argv[3]
 const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545")
 
 const BYTES_PER_FIELD_ELEMENT = 32
-const FIELD_ELEMENTS_PER_BLOB = 1016
+const FIELD_ELEMENTS_PER_BLOB = 4096
 const USEFUL_BYTES_PER_BLOB = 32 * FIELD_ELEMENTS_PER_BLOB
 const MAX_BLOBS_PER_TX = 2
 const MAX_USEFUL_BYTES_PER_TX = (USEFUL_BYTES_PER_BLOB * MAX_BLOBS_PER_TX) - 1
@@ -35,7 +36,7 @@ function get_blob(data) {
 function get_blobs(data) {
     data = Buffer.from(data, 'binary')
     const len = Buffer.byteLength(data)
-    if (len == 0) {
+    if (len === 0) {
         throw Error("invalid blob data")
     }
     if (len > MAX_USEFUL_BYTES_PER_TX) {
@@ -73,7 +74,7 @@ async function estimateGas(tx) {
     return res.data.result
 }
 
-async function run(data) {
+async function run(data, expected_kzgs) {
     while (true) {
         const num = await provider.getBlockNumber()
         if (num >= 9) {
@@ -84,7 +85,7 @@ async function run(data) {
     }
     let blobs = get_blobs(data)
     console.log("number of blobs is " + blobs.length)
-    const blobshex = blobs.map((x) => { x.toString('hex') })
+    const blobshex = blobs.map(x => `0x${x.toString("hex")}`)
 
     const account = ethers.Wallet.createRandom()
     const txData = {
@@ -106,7 +107,11 @@ async function run(data) {
     const res = await axios.post("http://localhost:8545", req)
     console.log(res.data)
     if (res.data.error) {
-        process.exit(1)
+        return false
+    }
+
+    if (expected_kzgs === undefined) {
+        return true
     }
 
     let blob_kzg = null
@@ -122,21 +127,23 @@ async function run(data) {
                 if (current > start + i) {
                     break
                 }
-                console.log(`waiting for next block.... bn=${current}`)
+                console.log(`waiting for tx to be included in block.... bn=${current}`)
                 await sleep(1000)
             }
         }
     } catch(error) {
         console.log(`Error retrieving blocks from ${error.config.url}: ${error.response.data}`)
-        process.exit(1)
+        return false
     }
 
-    if (blob_kzg !== "0xc00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") {
-        console.log(`Unexpected KZG value: ${blob_kzg}`)
-        process.exit(1)
+    if (blob_kzg !== expected_kzgs) {
+        console.log(`Unexpected KZG value: expected ${expected_kzgs}, got ${blob_kzg}`)
+        return false
     } else {
         console.log(`Found expected KZG value: ${blob_kzg}`)
     }
+
+    return true
 }
 
-(async () => { run(input) })()
+(async () => { process.exit((await run(input, expected_kzgs)) ? 0 : 1) })()
