0acd4336903de957f968e3a7c30fdeaccab104ee
inphi <mlaw2501@gmail.com>
2022-10-06 23:18:31 -0400

    Add historical sync test
    Asserts that the beacon node follower can sync blobs during initial-sync.
    This patch also refactors the test harness to accommodate dynamic nodes.

---
+++ tests/ctrl/services.go
@@ -0,0 +1,132 @@
+package ctrl
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/Inphi/eip4844-interop/shared"
+	"github.com/ethereum/go-ethereum/ethclient"
+	beaconservice "github.com/prysmaticlabs/prysm/v3/proto/eth/service"
+	"google.golang.org/grpc"
+)
+
+type Service interface {
+	Start(ctx context.Context) error
+	Stop(ctx context.Context) error
+	Started() <-chan struct{}
+}
+
+func NewBeaconNode() Service {
+	url := fmt.Sprintf("http://%s/eth/v1/beacon/genesis", shared.BeaconGatewayGRPC)
+	return newDockerService("beacon-node", url)
+}
+
+func NewValidatorNode() Service {
+	return newDockerService("validator-node", shared.ValidatorRPC)
+}
+
+func GetBeaconNodeClient(ctx context.Context) (beaconservice.BeaconChainClient, error) {
+	// TODO: cache conns for reuse
+	conn, err := grpc.DialContext(ctx, shared.BeaconRPC, grpc.WithInsecure())
+	if err != nil {
+		return nil, fmt.Errorf("%w: failed to dial beacon grpc", err)
+	}
+	return beaconservice.NewBeaconChainClient(conn), nil
+}
+
+func NewBeaconNodeFollower() Service {
+	url := fmt.Sprintf("http://%s/eth/v1/beacon/genesis", shared.BeaconGatewayFollowerGRPC)
+	return newDockerService("beacon-node-follower", url)
+}
+
+func GetBeaconNodeFollowerClient(ctx context.Context) (beaconservice.BeaconChainClient, error) {
+	// TODO: cache conns for reuse
+	conn, err := grpc.DialContext(ctx, shared.BeaconFollowerRPC, grpc.WithInsecure())
+	if err != nil {
+		return nil, fmt.Errorf("%w: failed to dial beacon follower grpc", err)
+	}
+	return beaconservice.NewBeaconChainClient(conn), nil
+}
+
+func NewGethNode() Service {
+	return newDockerService("execution-node", shared.GethRPC)
+}
+
+func NewGethNode2() Service {
+	return newDockerService("execution-node-2", shared.GethRPC)
+}
+
+func GetExecutionClient(ctx context.Context) (*ethclient.Client, error) {
+	client, err := ethclient.DialContext(ctx, shared.GethRPC)
+	if err != nil {
+		return nil, fmt.Errorf("%w: Failed to connect to the Ethereum client", err)
+	}
+	return client, nil
+}
+
+type dockerService struct {
+	started   chan struct{}
+	svcname   string
+	statusURL string
+}
+
+func (s *dockerService) Start(ctx context.Context) error {
+	if err := StartServices(s.svcname); err != nil {
+		return err
+	}
+	req, err := http.NewRequestWithContext(ctx, "GET", s.statusURL, nil)
+	if err != nil {
+		return err
+	}
+	for {
+		if _, err := http.DefaultClient.Do(req); err == nil {
+			close(s.started)
+			return nil
+		}
+		if err := ctx.Err(); err != nil {
+			return err
+		}
+	}
+}
+
+func (s *dockerService) Stop(ctx context.Context) error {
+	return StopService(s.svcname)
+}
+
+func (s *dockerService) Started() <-chan struct{} {
+	return s.started
+}
+
+func (s *dockerService) updateStarted() {
+	ctx := context.Background()
+	req, err := http.NewRequestWithContext(ctx, "GET", s.statusURL, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		if _, err := http.DefaultClient.Do(req); err == nil {
+			close(s.started)
+		}
+	}
+}
+
+func ServiceReady(ctx context.Context, svc Service) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-svc.Started():
+			return nil
+		}
+	}
+}
+
+func newDockerService(svcname string, statusURL string) Service {
+	return &dockerService{
+		started:   make(chan struct{}),
+		svcname:   svcname,
+		statusURL: statusURL,
+	}
+}
--- tests/fee-market/main.go
+++ tests/fee-market/main.go
@@ -3,37 +3,26 @@ package main
 import (
 	"bytes"
 	"context"
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"math/big"
 	"sort"
-	"strings"
 	"sync"
 	"time"
 
 	"github.com/Inphi/eip4844-interop/shared"
 	"github.com/Inphi/eip4844-interop/tests/ctrl"
+	"github.com/Inphi/eip4844-interop/tests/util"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus/misc"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/holiman/uint256"
-	"github.com/libp2p/go-libp2p"
-	libp2pcore "github.com/libp2p/go-libp2p-core"
-	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-core/protocol"
-	ma "github.com/multiformats/go-multiaddr"
 	"github.com/protolambda/ztyp/view"
-	"github.com/prysmaticlabs/prysm/v3/beacon-chain/p2p"
-	"github.com/prysmaticlabs/prysm/v3/beacon-chain/p2p/encoder"
-	beaconchainsync "github.com/prysmaticlabs/prysm/v3/beacon-chain/sync"
 	consensustypes "github.com/prysmaticlabs/prysm/v3/consensus-types/primitives"
-	ethpbv1 "github.com/prysmaticlabs/prysm/v3/proto/eth/v1"
+	"github.com/prysmaticlabs/prysm/v3/proto/eth/service"
 	ethpbv2 "github.com/prysmaticlabs/prysm/v3/proto/eth/v2"
-	ethpb "github.com/prysmaticlabs/prysm/v3/proto/prysm/v1alpha1"
 )
 
 func GetBlob() types.Blobs {
@@ -49,9 +38,19 @@ func main() {
 	ctrl.InitE2ETest()
 	ctrl.WaitForShardingFork()
 	ctrl.WaitForEip4844ForkEpoch()
+	env := ctrl.GetEnv()
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*20)
 	defer cancel()
+
+	ethClient, err := ctrl.GetExecutionClient(ctx)
+	if err != nil {
+		log.Fatalf("unable to get execution client: %v", err)
+	}
+	beaconClient, err := ctrl.GetBeaconNodeClient(ctx)
+	if err != nil {
+		log.Fatalf("unable to get beacon client: %v", err)
+	}
 
 	blobsData := make([]types.Blobs, 20)
 	for i := range blobsData {
@@ -59,20 +58,20 @@ func main() {
 	}
 
 	// Retrieve the current slot to being our blobs search on the beacon chain
-	startSlot := GetHeadSlot(ctx)
+	startSlot := util.GetHeadSlot(ctx, beaconClient)
 
 	// Send multiple transactions at the same time to induce non-zero excess_blobs
-	UploadBlobsAndCheckBlockHeader(ctx, blobsData)
+	UploadBlobsAndCheckBlockHeader(ctx, ethClient, blobsData)
 
-	WaitForNextSlot(ctx)
-	WaitForNextSlot(ctx)
+	util.WaitForNextSlots(ctx, beaconClient, 1)
+	util.WaitForNextSlots(ctx, beaconClient, 1)
 
-	blocks := FindBlocksWithBlobs(ctx, startSlot)
+	blocks := FindBlocksWithBlobs(ctx, beaconClient, startSlot)
 
 	log.Printf("checking blob from beacon node")
 	var downloadedData []byte
 	for _, b := range blocks {
-		data := DownloadBlobs(ctx, b.Slot, 1, shared.BeaconMultiAddress)
+		data := util.DownloadBlobs(ctx, b.Slot, 1, shared.BeaconMultiAddress)
 		downloadedData = append(downloadedData, data...)
 	}
 
@@ -83,11 +82,11 @@ func main() {
 	}
 
 	log.Printf("checking blob from beacon node follower")
-	time.Sleep(time.Second * 2 * time.Duration(ctrl.Env.BeaconChainConfig.SecondsPerSlot)) // wait a bit for sync
+	time.Sleep(time.Second * 2 * time.Duration(env.BeaconChainConfig.SecondsPerSlot)) // wait a bit for sync
 
 	downloadedData = nil
 	for _, b := range blocks {
-		data := DownloadBlobs(ctx, b.Slot, 1, shared.BeaconFollowerMultiAddress)
+		data := util.DownloadBlobs(ctx, b.Slot, 1, shared.BeaconFollowerMultiAddress)
 		downloadedData = append(downloadedData, data...)
 	}
 	if !bytes.Equal(flatBlobs, downloadedData) {
@@ -110,22 +109,7 @@ func FlattenBlobs(blobsData []types.Blobs) []byte {
 	return out
 }
 
-func WaitForNextSlot(ctx context.Context) {
-	if err := ctrl.WaitForSlot(ctx, GetHeadSlot(ctx).Add(1)); err != nil {
-		log.Fatalf("error waiting for next slot: %v", err)
-	}
-}
-
-func GetHeadSlot(ctx context.Context) consensustypes.Slot {
-	req := &ethpbv1.BlockRequest{BlockId: []byte("head")}
-	header, err := ctrl.Env.BeaconChainClient.GetBlockHeader(ctx, req)
-	if err != nil {
-		log.Fatalf("unable to get beacon chain head: %v", err)
-	}
-	return header.Data.Header.Message.Slot
-}
-
-func UploadBlobsAndCheckBlockHeader(ctx context.Context, blobsData []types.Blobs) {
+func UploadBlobsAndCheckBlockHeader(ctx context.Context, client *ethclient.Client, blobsData []types.Blobs) {
 	chainId := big.NewInt(1)
 	signer := types.NewDankSigner(chainId)
 
@@ -134,7 +118,7 @@ func UploadBlobsAndCheckBlockHeader(ctx context.Context, blobsData []types.Blobs
 		log.Fatalf("Failed to load private key: %v", err)
 	}
 
-	nonce, err := ctrl.Env.EthClient.PendingNonceAt(ctx, crypto.PubkeyToAddress(key.PublicKey))
+	nonce, err := client.PendingNonceAt(ctx, crypto.PubkeyToAddress(key.PublicKey))
 	if err != nil {
 		log.Fatalf("Error getting nonce: %v", err)
 	}
@@ -179,14 +163,14 @@ func UploadBlobsAndCheckBlockHeader(ctx context.Context, blobsData []types.Blobs
 		tx := tx
 		go func() {
 			defer wg.Done()
-			err := ctrl.Env.EthClient.SendTransaction(ctx, tx)
+			err := client.SendTransaction(ctx, tx)
 			if err != nil {
 				log.Fatalf("Error sending tx: %v", err)
 			}
 
 			log.Printf("Waiting for transaction (%v) to be included...", tx.Hash())
 
-			receipt, err := shared.WaitForReceipt(ctx, ctrl.Env.EthClient, tx.Hash())
+			receipt, err := shared.WaitForReceipt(ctx, client, tx.Hash())
 			if err != nil {
 				log.Fatalf("Error waiting for transaction receipt %v: %v", tx.Hash(), err)
 			}
@@ -203,7 +187,7 @@ func UploadBlobsAndCheckBlockHeader(ctx context.Context, blobsData []types.Blobs
 		blocknum := receipt.BlockNumber.Uint64()
 		if _, ok := blockNumbers[blocknum]; !ok {
 			blockHash := receipt.BlockHash.Hex()
-			block, err := ctrl.Env.EthClient.BlockByHash(ctx, common.HexToHash(blockHash))
+			block, err := client.BlockByHash(ctx, common.HexToHash(blockHash))
 			if err != nil {
 				log.Fatalf("Error getting block: %v", err)
 			}
@@ -220,7 +204,7 @@ func UploadBlobsAndCheckBlockHeader(ctx context.Context, blobsData []types.Blobs
 	})
 
 	prevExcessDataGas := new(big.Int)
-	parentBlock, err := ctrl.Env.EthClient.BlockByHash(ctx, blocks[0].ParentHash())
+	parentBlock, err := client.BlockByHash(ctx, blocks[0].ParentHash())
 	if err != nil {
 		log.Fatalf("Error getting block: %v", err)
 	}
@@ -238,9 +222,9 @@ func UploadBlobsAndCheckBlockHeader(ctx context.Context, blobsData []types.Blobs
 	}
 }
 
-func FindBlocksWithBlobs(ctx context.Context, startSlot consensustypes.Slot) []*ethpbv2.BeaconBlockEip4844 {
+func FindBlocksWithBlobs(ctx context.Context, client service.BeaconChainClient, startSlot consensustypes.Slot) []*ethpbv2.BeaconBlockEip4844 {
 	slot := startSlot
-	endSlot := GetHeadSlot(ctx)
+	endSlot := util.GetHeadSlot(ctx, client)
 
 	var blocks []*ethpbv2.BeaconBlockEip4844
 	for {
@@ -250,7 +234,7 @@ func FindBlocksWithBlobs(ctx context.Context, startSlot consensustypes.Slot) []*
 
 		blockID := fmt.Sprintf("%d", uint64(slot))
 		req := &ethpbv2.BlockRequestV2{BlockId: []byte(blockID)}
-		block, err := ctrl.Env.BeaconChainClient.GetBlockV2(ctx, req)
+		block, err := client.GetBlockV2(ctx, req)
 		if err != nil {
 			log.Fatalf("beaconchainclient.GetBlock: %v", err)
 		}
@@ -268,151 +252,4 @@ func FindBlocksWithBlobs(ctx context.Context, startSlot consensustypes.Slot) []*
 		log.Fatalf("Unable to find beacon block containing blobs")
 	}
 	return blocks
-}
-
-func DownloadBlobs(ctx context.Context, startSlot consensustypes.Slot, count uint64, beaconMA string) []byte {
-	// TODO: Use Beacon gRPC to download blobs rather than p2p RPC
-	log.Print("downloading blobs...")
-
-	req := &ethpb.BlobsSidecarsByRangeRequest{
-		StartSlot: startSlot,
-		Count:     count,
-	}
-
-	h, err := libp2p.New()
-	if err != nil {
-		log.Fatalf("failed to create libp2p context: %v", err)
-	}
-	defer func() {
-		_ = h.Close()
-	}()
-
-	multiaddr, err := getMultiaddr(ctx, h, beaconMA)
-	if err != nil {
-		log.Fatalf("getMultiAddr: %v", err)
-	}
-
-	addrInfo, err := peer.AddrInfoFromP2pAddr(multiaddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = h.Connect(ctx, *addrInfo)
-	if err != nil {
-		log.Fatalf("libp2p host connect: %v", err)
-	}
-
-	// Hack to ensure that we are able to download blob chunks with larger chunk sizes (which is 10 MiB post-bellatrix)
-	encoder.MaxChunkSize = 10 << 20
-	sidecars, err := sendBlobsSidecarsByRangeRequest(ctx, h, encoder.SszNetworkEncoder{}, addrInfo.ID, req)
-	if err != nil {
-		log.Fatalf("failed to send blobs p2p request: %v", err)
-	}
-
-	anyBlobs := false
-	blobsBuffer := new(bytes.Buffer)
-	for _, sidecar := range sidecars {
-		log.Printf("found sidecar with %d blobs", len(sidecar.Blobs))
-		if sidecar.Blobs == nil || len(sidecar.Blobs) == 0 {
-			continue
-		}
-		anyBlobs = true
-		for _, blob := range sidecar.Blobs {
-			data := shared.DecodeBlob(blob.Blob)
-			_, _ = blobsBuffer.Write(data)
-		}
-
-		// stop after the first sidecar with blobs:
-		break
-	}
-
-	if !anyBlobs {
-		log.Fatalf("No blobs found in requested slots, sidecar count: %d", len(sidecars))
-	}
-
-	return blobsBuffer.Bytes()
-}
-
-func getMultiaddr(ctx context.Context, h host.Host, addr string) (ma.Multiaddr, error) {
-	multiaddr, err := ma.NewMultiaddr(addr)
-	if err != nil {
-		return nil, err
-	}
-	_, id := peer.SplitAddr(multiaddr)
-	if id != "" {
-		return multiaddr, nil
-	}
-	// peer ID wasn't provided, look it up
-	id, err = retrievePeerID(ctx, h, addr)
-	if err != nil {
-		return nil, err
-	}
-	return ma.NewMultiaddr(fmt.Sprintf("%s/p2p/%s", addr, string(id)))
-}
-
-// Helper for retrieving the peer ID from a security error... obviously don't use this in production!
-// See https://github.com/libp2p/go-libp2p-noise/blob/v0.3.0/handshake.go#L250
-func retrievePeerID(ctx context.Context, h host.Host, addr string) (peer.ID, error) {
-	incorrectPeerID := "16Uiu2HAmSifdT5QutTsaET8xqjWAMPp4obrQv7LN79f2RMmBe3nY"
-	addrInfo, err := peer.AddrInfoFromString(fmt.Sprintf("%s/p2p/%s", addr, incorrectPeerID))
-	if err != nil {
-		return "", err
-	}
-	err = h.Connect(ctx, *addrInfo)
-	if err == nil {
-		return "", errors.New("unexpected successful connection")
-	}
-	if strings.Contains(err.Error(), "but remote key matches") {
-		split := strings.Split(err.Error(), " ")
-		return peer.ID(split[len(split)-1]), nil
-	}
-	return "", err
-}
-
-func sendBlobsSidecarsByRangeRequest(ctx context.Context, h host.Host, encoding encoder.NetworkEncoding, pid peer.ID, req *ethpb.BlobsSidecarsByRangeRequest) ([]*ethpb.BlobsSidecar, error) {
-	topic := fmt.Sprintf("%s%s", p2p.RPCBlobsSidecarsByRangeTopicV1, encoding.ProtocolSuffix())
-
-	stream, err := h.NewStream(ctx, pid, protocol.ID(topic))
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		_ = stream.Close()
-	}()
-
-	if _, err := encoding.EncodeWithMaxLength(stream, req); err != nil {
-		_ = stream.Reset()
-		return nil, err
-	}
-
-	if err := stream.CloseWrite(); err != nil {
-		_ = stream.Reset()
-		return nil, err
-	}
-
-	var blobsSidecars []*ethpb.BlobsSidecar
-	for {
-		blobs, err := readChunkedBlobsSidecar(stream, encoding)
-		if errors.Is(err, io.EOF) {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-		blobsSidecars = append(blobsSidecars, blobs)
-	}
-	return blobsSidecars, nil
-}
-
-func readChunkedBlobsSidecar(stream libp2pcore.Stream, encoding encoder.NetworkEncoding) (*ethpb.BlobsSidecar, error) {
-	code, errMsg, err := beaconchainsync.ReadStatusCode(stream, encoding)
-	if err != nil {
-		return nil, err
-	}
-	if code != 0 {
-		return nil, errors.New(errMsg)
-	}
-	sidecar := new(ethpb.BlobsSidecar)
-	err = encoding.DecodeWithMaxLength(stream, sidecar)
-	return sidecar, err
 }
