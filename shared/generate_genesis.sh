a95c23bb7eb4b3824427e5899eda3b2b84450b32
Francis Li <francis.li@coinbase.com>
2023-01-14 21:09:44 -0800

    Finish build-shared-states container

--- shared/generate_genesis.sh
+++ shared/generate_genesis.sh
@@ -4,5 +4,5 @@ GENESIS=$(($(date +%s) + 60))   # 60s till genesis
 SHANGHAI=$(($GENESIS + 108))    # 108s till shanghai
 CANCUN=$(($GENESIS + 144))      # 144s till cancun
 
-sed -i '' 's/XXX/'$SHANGHAI'/'  ./genesis.json
-sed -i '' 's/YYY/'$CANCUN'/'    ./genesis.json
+sed -i -e 's/XXX/'$SHANGHAI'/'  ./genesis.json
+sed -i -e 's/YYY/'$CANCUN'/'    ./genesis.json
