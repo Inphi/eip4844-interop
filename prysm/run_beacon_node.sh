de8478957d549f62c7d1f0004640dab58a863910
inphi <mlaw2501@gmail.com>
2023-01-23 12:10:31 -0500

    fix prysm test

--- prysm/run_beacon_node.sh
+++ prysm/run_beacon_node.sh
@@ -6,13 +6,13 @@ set -exu -o pipefail
 : "${PROCESS_NAME:-beacon-node}"
 : "${TRACING_ENDPOINT:-}"
 : "${VERBOSITY:-info}"
-: "${P2P_PRIV_KEY:-}"
 : "${P2P_TCP_PORT:-13000}"
 : "${MIN_SYNC_PEERS:-0}"
 
 EXTERNAL_IP=$(ip addr show eth0 | grep inet | awk '{ print $2 }' | cut -d '/' -f1)
 
 BOOTNODE=$(cat /config_data/custom_config_data/boot_enr.yaml | sed 's/- //')
+openssl rand -hex 32 | tr -d '\n' > /tmp/priv-key
 
 beacon-chain\
     --accept-terms-of-use \
@@ -35,7 +35,7 @@ beacon-chain\
     --p2p-local-ip 0.0.0.0 \
     --p2p-host-ip "$EXTERNAL_IP" \
     --p2p-tcp-port $P2P_TCP_PORT \
-    --p2p-priv-key="$P2P_PRIV_KEY" \
+    --p2p-priv-key=/tmp/priv-key \
     --suggested-fee-recipient=0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b \
     --subscribe-all-subnets \
     --enable-tracing \
